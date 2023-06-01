994b76ecf816e4f73165aa47ffd8e3ab5f3ab700
kaurkomal10 <kau14953@sheridancollege.ca>
2023-06-01 00:12:27 -0400

    Updated code

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
-
+import java.util.Scanner;
+import java.util.Random;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -13,16 +14,37 @@ package card;
 public class CardTrick {
     
     public static void main(String[] args)
-    {
+{
         Card[] magicHand = new Card[7];
+        Random kk = new Random();
+        Scanner komal = new Scanner(System.in);
         
         for (int i=0; i<magicHand.length; i++)
-        {
+            {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(kk.nextInt(13)+1);
+            c.setSuit(Card.SUITS[kk.nextInt(4)]);
+            magicHand[i] = c;
+            }
+            System.out.println("Enter the card nummber:");
+            int cardnumber = komal.nextInt();
+            System.out.println("Enter the suits:");
+            String Suits = komal.next();
+            
+            boolean value = false;
+            
+            for(Card CardGames : magicHand){
+                if(CardGames.getValue() == cardnumber && CardGames.getSuit().equalsIgnoreCase(Suits)){
+                    value = true;
+                    break;
+                }
         }
         
+            if(value){
+                System.out.println("The card number and suit is present in the array:");
+            }else{
+                System.out.println("The card number and suit is not present in the array:");
+            }
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
